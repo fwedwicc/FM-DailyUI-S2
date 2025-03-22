5704d8428b6c9820ba8657df9ea3dd1f59854986
fwedwicc <moreno.frederick.capiral@gmail.com>
2025-03-22 23:33:13 +0800

    feat: enhance Hero component with additional sections and links for FM DailyUI

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <section className='bg-indigo-100'>
-      <h1>Hero Section</h1>
-    </section>
+    <>
+      <section className='bg-indigo-100 md:min-h-screen'>
+        <h1>Hero Section</h1>
+      </section>
+      <section className='bg-red-100'>
+        <a href="https://fwedwicc.github.io/FM-DailyUI/" target='_blank' rel='noopener noreferrer' className='inline-block border p-4'>
+          FM DailyUI S1
+        </a>
+        <div className='inline-block border p-4'>
+          FM DailyUI S2
+          Upcoming Challenge
+        </div>
+        <div className='inline-block border p-4'>
+          FM DailyUI S3
+          Upcoming Challenge
+        </div>
+        <div className='inline-block border p-4'>
+          FM DailyUI S4
+          Upcoming Challenge
+        </div>
+      </section>
+    </>
   )
 }
 
