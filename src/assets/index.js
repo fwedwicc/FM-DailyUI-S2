6a596200d3fb5f718f10f8ed60e7673c77a380bc
fwedwicc <moreno.frederick.capiral@gmail.com>
2025-03-28 22:05:01 +0800

    feat: add VRGlass asset and update Day27 page with new layout and illustration

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,9 @@
 import FMLogo from './FM-logo.png'
 import FMIllustration from './FM-illustration.png'
+import VRGlass from './vr-glass.gif'
 
 export {
   FMLogo,
-  FMIllustration
+  FMIllustration,
+  VRGlass
 }
--- src/pages/Day27.jsx
+++ src/pages/Day27.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { VRGlass } from '../assets'
 import { motion } from 'framer-motion'
 
 const Day27 = () => {
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -9,7 +11,12 @@ const Day27 = () => {
       exit={{ opacity: 0 }}
       transition={{ duration: 0.5 }}
     >
-      <h1>Currently Crafting...</h1>
+      <main>
+        <section className='h-screen flex flex-col justify-center items-center'>
+          <img src={VRGlass} alt="VR Glass Illustration" className='' />
+          <h3 className='absolute bottom-1/3'>Still Crafting...</h3>
+        </section>
+      </main>
     </motion.div>
   )
 }
