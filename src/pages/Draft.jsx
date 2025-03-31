0c6b5dd5e5214da5dd7c64da029c6997e060fa7a
fwedwicc <moreno.frederick.capiral@gmail.com>
2025-03-31 22:10:11 +0800

    feat: update Draft page with new layout, dropdown menu, and FMIllustration asset

--- src/pages/Draft.jsx
+++ src/pages/Draft.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { motion } from 'framer-motion'
-import { FMLogo } from '../assets'
+import { FMLogo, FMIllustration } from '../assets'
 import { Button, Badge } from '../components/ui'
-import { HiChevronDown, HiChevronUp } from "react-icons/hi"
+import { HiChevronDown, HiChevronUp, HiOutlineStar } from "react-icons/hi"
 
 const Draft = () => {
   return (
@@ -12,14 +12,14 @@ const Draft = () => {
       exit={{ opacity: 0 }}
       transition={{ duration: 0.5 }}
     >
-      <div className='lg:min-h-screen border border-yellow-300/40'>
+      <div className='lg:min-h-screen'>
         <div className='flex flex-col items-center justify-between p-8 space-y-4'>
           {/* Navigation Bar */}
-          <nav className='flex items-center justify-between w-full max-w-7xl rounded-xl p-3 bg-gray-800/40 border border-yellow-300/40'>
+          <nav className='flex items-center justify-between w-full max-w-7xl rounded-xl p-4 border border-gray-800 bg-gray-800/40'>
             {/* Start */}
             <img src={FMLogo} alt="FM Logo" className='w-7 ms-2' />
             {/* Middle */}
-            <div className='md:flex hidden items-center gap-2 border border-yellow-300/40'>
+            <div className='md:flex hidden items-center gap-2'>
               <Button label={'Primary'} variant='ghost' />
               <Button label={'Primary'} variant='secondary'>
                 <HiChevronUp className='size-5' />
@@ -30,17 +30,46 @@ const Draft = () => {
               <Button label={'Primary'} variant='ghost' />
             </div>
             {/* End */}
-            <div className='md:flex hidden items-center gap-2 border border-yellow-300/40'>
+            <div className='md:flex hidden items-center gap-2'>
               <Badge text='Ctrl K' styles='me-3 cursor-pointer' />
               <Button label={'Secondary'} variant='secondary' />
               <Button label={'Primary'} variant='primary' />
             </div>
           </nav>
           {/* Dropdown Menu */}
-          <div className='grid grid-cols-3 w-full max-w-4xl bg-gray-800/40 p-4 rounded-xl border border-yellow-300/40'>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, alias error. Sit ad pariatur libero eveniet corrupti tempora in dolores aspernatur, autem, enim temporibus eos ipsum? Sit incidunt perspiciatis debitis?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, alias error. Sit ad pariatur libero eveniet corrupti tempora in dolores aspernatur, autem, enim temporibus eos ipsum? Sit incidunt perspiciatis debitis?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, alias error. Sit ad pariatur libero eveniet corrupti tempora in dolores aspernatur, autem, enim temporibus eos ipsum? Sit incidunt perspiciatis debitis?</p>
+          <div className='grid md:grid-cols-3 grid-cols-1 gap-3 w-full max-w-5xl border border-gray-800 bg-gray-800/40 p-4 rounded-2xl'>
+            {/* First and Second Column */}
+            <div className='self-start col-span-2 grid grid-cols-2 gap-3'>
+              {[
+                { icon: HiOutlineStar, title: 'Hello World', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit, error.' },
+                { icon: HiOutlineStar, title: 'Hello World', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit, error.' },
+                { icon: HiOutlineStar, title: 'Hello World', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit, error.' },
+                { icon: HiOutlineStar, title: 'Hello World', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit, error.' },
+                { icon: HiOutlineStar, title: 'Hello World', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit, error.' },
+                { icon: HiOutlineStar, title: 'Hello World', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit, error.' },
+              ].map((item, index) => (
+                <a className='border border-gray-700/40 hover:bg-gray-800/50 px-4 py-3 space-y-1 cursor-pointer rounded-lg transition duration-300 ease-in-out' key={index}>
+                  <div className='flex items-center gap-1'>
+                    <item.icon className='size-6 text-gray-100' />
+                    <h5>{item.title}</h5>
+                  </div>
+                  <p>{item.desc}</p>
+                </a>
+              ))}
+            </div>
+            {/* Third Column */}
+            <div className='space-y-2'>
+              <div className='relative w-full h-36 border border-gray-800 rounded-2xl overflow-hidden'>
+                <img src={FMIllustration} alt="FM Illustration" className='object-cover w-full h-full absolute' />
+              </div>
+              <div className='space-y-1'>
+                <h3>Hello, World!</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, alisectetur adipisicing elit. Sas error. Sit ad pariatur libero eveniet corrupti tem.</p>
+                <Button label={'Primary'} variant='primary' styles='mt-3'>
+                  <HiOutlineStar className='size-5' />
+                </Button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
