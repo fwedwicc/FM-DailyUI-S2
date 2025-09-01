7cde9d46b8cc72a4cafac705988c3f446be3bf9a
fwedwicc <moreno.frederick.capiral@gmail.com>
2025-09-01 14:30:26 +0800

    feat: enhance PhoneMockup component with new design elements and icons for improved visual appeal

--- src/pages/Draft.jsx
+++ src/pages/Draft.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import useScrollToTop from '../hooks/useScrollToTop'
+import { TbBattery1 } from "react-icons/tb"
+import { IoIosWifi } from "react-icons/io"
+import { HiChartBar } from "react-icons/hi"
 
 const Draft = () => {
 
@@ -8,31 +11,38 @@ const Draft = () => {
 
   const PhoneMockup = ({ children }) => {
     return (
-      <div className='relative border-[0.5rem] h-[40rem] w-[21.5rem] rounded-[2rem] ring-2 z-50 bg-gray-700/40 border-gray-900 ring-gray-400'>
-        {/* Sound Buttons */}
-        <div className='absolute top-20 -left-3 flex flex-col gap-3'>
+      <div className='relative bg-gray-400 p-[3.2px] h-[40rem] w-[21.5rem] rounded-[48px]'>
+        {/* Kempamparus */}
+        {['right-16 top-0', 'left-0 top-16', 'right-0 top-16', 'left-0 bottom-16', 'right-0 bottom-16', 'left-16 bottom-0'].map((item, index) => (
+          <div className={`absolute size-1 bg-gray-600 ${item}`} key={index} />
+        ))}
+        {/* Left Buttons */}
+        <div className='absolute top-30 -left-[2px] flex flex-col gap-3'>
           {[0, 1, 2].map((index) => (
-            <div
-              className={`rounded-full w-2 bg-gray-900 border-l-2 border-gray-400 ${index === 0 ? 'h-7' : 'h-10'}`} key={index}>
-            </div>
+            <div className={`rounded-l-[1px] w-[2px] bg-gray-400 border-r border-gray-800 ${index === 0 ? 'h-7' : 'h-10'}`} key={index} />
           ))}
         </div>
         {/* Power Button */}
-        <div className='absolute top-[9.2rem] border-gray-400 border-r-2 -right-3 rounded-full h-16 w-2 bg-gray-900' />
-        {/* Camera */}
-        <div className='absolute rounded-full h-[1.3rem] w-20 top-3 left-1/2 transform -translate-x-1/2 bg-gray-900' />
-        {/* Phone Header */}
-        <div className='absolute w-full flex justify-between items-center pt-2.5 px-3.5 text-white'>
-          <span className="leading-relaxed text-xs font-semibold">4:17</span>
-          <div className='flex items-center gap-1.5'>
-            {['M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75ZM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 0 1-1.875-1.875V8.625ZM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 0 1 3 19.875v-6.75Z', 'M1.371 8.143c5.858-5.857 15.356-5.857 21.213 0a.75.75 0 0 1 0 1.061l-.53.53a.75.75 0 0 1-1.06 0c-4.98-4.979-13.053-4.979-18.032 0a.75.75 0 0 1-1.06 0l-.53-.53a.75.75 0 0 1 0-1.06Zm3.182 3.182c4.1-4.1 10.749-4.1 14.85 0a.75.75 0 0 1 0 1.061l-.53.53a.75.75 0 0 1-1.062 0 8.25 8.25 0 0 0-11.667 0 .75.75 0 0 1-1.06 0l-.53-.53a.75.75 0 0 1 0-1.06Zm3.204 3.182a6 6 0 0 1 8.486 0 .75.75 0 0 1 0 1.061l-.53.53a.75.75 0 0 1-1.061 0 3.75 3.75 0 0 0-5.304 0 .75.75 0 0 1-1.06 0l-.53-.53a.75.75 0 0 1 0-1.06Zm3.182 3.182a1.5 1.5 0 0 1 2.122 0 .75.75 0 0 1 0 1.061l-.53.53a.75.75 0 0 1-1.061 0l-.53-.53a.75.75 0 0 1 0-1.06Z', 'M3.75 6.75a3 3 0 0 0-3 3v6a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3v-.037c.856-.174 1.5-.93 1.5-1.838v-2.25c0-.907-.644-1.664-1.5-1.837V9.75a3 3 0 0 0-3-3h-15Zm15 1.5a1.5 1.5 0 0 1 1.5 1.5v6a1.5 1.5 0 0 1-1.5 1.5h-15a1.5 1.5 0 0 1-1.5-1.5v-6a1.5 1.5 0 0 1 1.5-1.5h15Z'].map((icon, index) => (
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`${index === 2 ? 'size-4' : 'size-3'}`} key={index}>
-                <path d={icon} />
-              </svg>
-            ))}
+        <div className='absolute top-[10.3rem] -right-[2px] rounded-r-[1px] w-[2px] bg-gray-400 border-l border-gray-800 h-18' />
+        {/* Phone */}
+        <div className='relative border-[0.5rem] size-full rounded-[46px] ring-2 z-50 bg-gray-800 border-neutral-950 ring-gray-700'>
+          {/* Speacker ba to?? */}
+          <div className='absolute -top-2 -translate-x-1/2 left-1/2 w-[3rem] h-[3px] rounded-b-full bg-neutral-900 z-50' />
+          {/* Camera */}
+          <div className='absolute rounded-full h-[1.3rem] w-19 top-2.5 left-1/2 transform -translate-x-1/2 bg-neutral-950 z-50'>
+            <div className='absolute right-1 -translate-y-1/2 top-1/2 size-3.5 rounded-full bg-black/30' />
           </div>
+          {/* Phone Header */}
+          <div className='absolute w-full flex justify-between items-center pt-3 px-6 text-white z-50'>
+            <span className="leading-relaxed text-xs font-semibold">4:17</span>
+            <div className='flex items-center gap-1'>
+              <HiChartBar className='size-3 me-0.5' />
+              <IoIosWifi className='size-3.5' />
+              <TbBattery1 className='size-5 stroke-[1.5px]' />
+            </div>
+          </div>
+          {children}
         </div>
-        {children}
       </div>
     )
   }
@@ -48,11 +58,18 @@ const Draft = () => {
       <div className='flex items-center justify-center md:flex-nowrap flex-wrap gap-12 lg:gap-20'>
         {/* First Mockup */}
         <PhoneMockup>
-
+          <div className='relative h-full overflow-hidden rounded-[39px]'>
+            <img src="https://placehold.co/30x30" alt="Sampol wolpepah" className='absolute object-cover size-full z-10' />
+            <div className='relative h-full w-full px-3.5 pt-12 z-50'>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae esse illum, ducimus laboriosam ipsam reiciendis quasi autem? Veritatis vel, commodi enim non consectetur quibusdam quos sit nihil impedit. Voluptas, perferendis.</p>
+            </div>
+          </div>
         </PhoneMockup>
         {/* Second Mockup */}
         <PhoneMockup>
-
+          <div className='relative h-full px-3.5 pt-12 overflow-hidden'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae esse illum, ducimus laboriosam ipsam reiciendis quasi autem? Veritatis vel, commodi enim non consectetur quibusdam quos sit nihil impedit. Voluptas, perferendis.</p>
+          </div>
         </PhoneMockup>
       </div>
     </motion.section>
